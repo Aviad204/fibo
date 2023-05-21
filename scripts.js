234a739a579752fa12a99ca3d0fb5b9b93112ae7
Aviad Shmuel <vocaviad@gmail.com>
2023-05-21 19:10:47 +0300

    add js file

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const numX = document.getElementById("numX")
+const numY = document.getElementById("numY")
+
+numX.innerText = 1
+numY.innerText = 2
+
+// innerText // innerHTML
+
+
+
+
+// 19:08!!!
